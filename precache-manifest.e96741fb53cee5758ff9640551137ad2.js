954c8ee916b7bef70f643a82a810f4cffcfa23d2
Rupesh-DRK <rupesh.drk21@gmail.com>
2024-03-13 11:50:05 +0530

    Updates

---
+++ precache-manifest.e96741fb53cee5758ff9640551137ad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825fba65720fe1a58a582652347e0a6c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "89ef4375d863ab1fe3cf",
+    "url": "/tic-tac-toe/static/css/main.70230fed.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/tic-tac-toe/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/tic-tac-toe/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "89ef4375d863ab1fe3cf",
+    "url": "/tic-tac-toe/static/js/main.74e42bc6.chunk.js"
+  },
+  {
+    "revision": "f0d3a54e9b54637cbde7",
+    "url": "/tic-tac-toe/static/js/runtime~main.e2e786e8.js"
+  }
+]);
